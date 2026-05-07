7832ae453e78cf4c7d50bd238984a7793b901815
Eber Urzeda dos Santos <eber.urzeda@gmail.com>
2026-05-07 12:22:37 +0200

    Criei aula Aula 13

---
+++ curso-python-estudonauta/modulo-02/aulas-m2/aulas_praticas/aula13.py
@@ -0,0 +1,18 @@
+#for c in range(0, 6):
+#    print(c)
+#print('Fim do teste')
+#
+#for c in range(6, 0, - 1):
+#    print(c)
+#print('Fim do teste')
+
+#n = int(input('Digite um número: '))
+#for c in range(0, n+1):
+#    print(c)
+#print('Fim!')
+
+s = 0
+for c in range(0, 4):
+    n = int(input('Digite um número: '))
+    s += n
+print(f'O somatório de todos os números foi {s}')
