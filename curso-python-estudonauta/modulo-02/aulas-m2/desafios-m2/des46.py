fa74da7c241aa826db8ce7955cbd00e683f2a6a8
Eber Urzeda dos Santos <eber.urzeda@gmail.com>
2026-05-08 15:07:33 +0200

    Criei do des46

---
+++ curso-python-estudonauta/modulo-02/aulas-m2/desafios-m2/des46.py
@@ -0,0 +1,37 @@
+"""from time import sleep
+print('Adeus Ano Velho!!!')
+print('10')
+sleep(1)
+print('9')
+sleep(1)
+print('8')
+sleep(1)
+print('7')
+sleep(1)
+print('6')
+sleep(1)
+print('5')
+sleep(1)
+print('4')
+sleep(1)
+print('3')
+sleep(1)
+print('2')
+sleep(1)
+print('1')
+sleep(1)
+
+print('Feliz Ano Novo')
+print('Pow - Pow - Bummm')"""
+
+from time import sleep
+
+print('Adeus Ano Velho!!!')
+
+for c in range(10, 0, -1):
+    print(c)
+    sleep(1)
+
+print('Feliz Ano Novo!!!')
+print('Pow - Pow - Bummm')
+
