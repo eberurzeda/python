ff4a05649f984711773b36684b55c897d5254452
Eber Urzeda dos Santos <eber.urzeda@gmail.com>
2026-05-10 17:26:21 +0200

    Atualização do desafio 56

--- curso-python-estudonauta/modulo-02/aulas-m2/desafios-m2/des56.py
+++ curso-python-estudonauta/modulo-02/aulas-m2/desafios-m2/des56.py
@@ -2,14 +2,25 @@ somaidade = 0
 mediaidade = 0
 maioridadehomem = 0
 nomevelho = 0
+totmulher20 = 0
 for p in range(1, 5):
     print(f'---- {p}ª pessoa ----')
     nome = str(input('Nome: ')).strip()
     idade = int(input('Idade: '))
     sexo = str(input('Sexo [M/F]: ')).strip()
     somaidade += idade
-    if p == 1:
+    if p == 1 and sexo in 'Mm':
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Mm' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Ff' and idade < 20:
+        totmulher20 += 1
+
         
 
 mediaidade = somaidade / 4
 print(f'A mádia de idade do gropo é de {mediaidade} anos.')
+print(f'O homem mais velho tem {maioridadehomem} anos e se chama {nomevelho}')
+print(f'Ao todo tem {totmulher20} mulher(s) com menos de 20 anos.' )
